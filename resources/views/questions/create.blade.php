e56fa487f12b64b2aab93bb0e282365793576b36
Serkan <serkanbulutgenc@gmail.com>
2020-03-03 21:34:22 +0300

    Implement form validation and store the question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -17,10 +17,11 @@
 
                     <div class="card-body">
                         <form action="{{route('questions.store')}}", method="POST">
+                            @method('Post')
                             @csrf
                             <div class="form-group">
                                 <label for="question-title">Question Title</label>
-                                <input type="text" id="question-title" class="form-control {{ $errors->has('title')? 'is-invalid':'' }}" name="title" >
+                                <input type="text" id="question-title" value="{{old('title')}}" class="form-control {{ $errors->has('title')? 'is-invalid':'' }}" name="title" >
                                 @if ( $errors->has('title'))
                                     <div class="invalid-feedback">
                                         <strong>{{ $errors->first('title')  }}</strong>
@@ -30,7 +31,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="question-body">Question Title</label>
-                                <textarea id="question-body" rows="10" class="form-control {{ $errors->has('body')? 'is-invalid':'' }}" name="body"></textarea>
+                                <textarea id="question-body" rows="10" class="form-control {{ $errors->has('body')? 'is-invalid':'' }}" name="body">{{old('body')}}"</textarea>
                                 @if ( $errors->has('body'))
                                     <div class="invalid-feedback">
                                         <strong>{{ $errors->first('body')  }}</strong>
