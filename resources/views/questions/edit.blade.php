834a8138513e9f5fc0fd4977703c6504c3725279
Serkan <serkanbulutgenc@gmail.com>
2020-03-04 21:12:47 +0300

    Extend the form functionality to be able update question

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -7,7 +7,7 @@
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex align-items-center">
-                            <h2>Ask Question</h2>
+                            <h2>Edit Question</h2>
                             <div class="ml-auto">
                                 <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all Question</a>
                             </div>
@@ -16,9 +16,11 @@
                     </div>
 
                     <div class="card-body">
-                        <form action="{{route('questions.store')}}", method="POST">
+                        <form action="{{route('questions.update',$question->id)}}" method="POST">
 
-                            @include('questions._form',['buttonText'=>'Ask Question'])
+                            {{ method_field('PUT') }}
+
+                            @include('questions._form',['buttonText'=>'Update Question'])
                         </form>
                     </div>
                 </div>
