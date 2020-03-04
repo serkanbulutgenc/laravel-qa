80cdd4101971bf9e0108c1e44e8943136de53c40
Serkan <serkanbulutgenc@gmail.com>
2020-03-04 21:30:37 +0300

    Implement delete functionality for the question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -97,5 +97,10 @@ class QuestionsController extends Controller
     public function destroy(Question $question)
     {
         //
+        $question->delete();
+
+        return  redirect()
+            ->route('questions.index')
+            ->with('success','Your question has been deleted');
     }
 }
