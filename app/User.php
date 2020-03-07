ea985bbf26da75a8c3efcc5697febf92309a158d
Serkan <serkanbulutgenc@gmail.com>
2020-03-07 22:23:23 +0300

    Display answers for question

--- app/User.php
+++ app/User.php
@@ -43,4 +43,11 @@ class User extends Authenticatable
         return "#";
 
     }
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 40;
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+    }
 }
