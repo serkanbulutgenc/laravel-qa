ff44e603653635ca4e3c9319ba2d512d9550e949
Serkan <serkanbulutgenc@gmail.com>
2020-03-08 16:26:23 +0300

    Display answers for question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,32 +5,39 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h1>{{$question->title}}</h1>
-                            <div class="ml-auto">
-                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
-                                    Question</a>
+                    <div class="card-body">
+
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h1>{{$question->title}}</h1>
+                                <div class="ml-auto">
+                                    <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
+                                        Question</a>
+                                </div>
                             </div>
+
                         </div>
 
-                    </div>
+                        <div class="media">
+                            <div class="media-body">
 
-                    <div class="card-body">
-                        {!! $question->body_html !!}
-                        <div class="float-right">
-                                        <span class="text-muted">
+                                {!! $question->body_html !!}
+                                <div class="float-right">
+                                <span class="text-muted">
                                             Answered {{$question->created_date}}
                                         </span>
-                            <div class="media mt-2">
-                                <a href="{{$question->user->url}}" class="pr-2">
-                                    <img src="{{$question->user->avatar}}">
-                                </a>
-                                <div class="media-body mt-1">
-                                    <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                    <div class="media mt-2">
+                                        <a href="{{$question->user->url}}" class="pr-2">
+                                            <img src="{{$question->user->avatar}}">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
@@ -68,5 +75,6 @@
                 </div>
             </div>
         </div>
+
     </div>
 @endsection
