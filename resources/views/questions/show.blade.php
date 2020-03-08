5a73789d4015a0d5a4b0a42788dca02dcd040bd5
Serkan <serkanbulutgenc@gmail.com>
2020-03-08 17:10:24 +0300

    Add vote control to question and answer item

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -17,8 +17,22 @@
                             </div>
 
                         </div>
+                        <hr>
 
                         <div class="media">
+                            <div class="d-flex flex-column votes-controls">
+                                <a title="This question is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1230</span>
+                                <a title="This question is not usef" class="votes-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                        <i class="fas fa-star fa-2x"></i>
+                                    <span class="favorites-count">123 </span>
+                                </a>
+                            </div>
                             <div class="media-body">
 
                                 {!! $question->body_html !!}
@@ -52,6 +66,19 @@
                         <hr>
                         @foreach ($question->answers as $answer)
                             <div class="media">
+                                <div class="d-flex flex-column votes-controls">
+                                    <a title="This answer is useful" class="vote-up">
+                                        <i class="fas fa-caret-up fa-3x"></i>
+                                    </a>
+                                    <span class="votes-count">1230</span>
+                                    <a title="This answer is not usef" class="votes-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                    </a>
+                                    <a title="Mark this answer as best answer" class="vote-accepted mt-2 ">
+                                        <i class="fas fa-check fa-2x"></i>
+                                        <span class="favorites-count">123 </span>
+                                    </a>
+                                </div>
                                 <div class="media-body">
                                     {!! $answer->body_html !!}
                                     <div class="float-right">
