3062a4fe6f8ce74df24d5314637ada60386582bd
Serkan <serkanbulutgenc@gmail.com>
2020-03-27 13:36:15 +0300

    Create additional api endpoint to display current users post

--- app/User.php
+++ app/User.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
@@ -15,20 +15,22 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable
+        = [
+            'name', 'email', 'password',
+        ];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden
+        = [
+            'password', 'remember_token',
+        ];
 
-    protected $appends =['url','avatar'];
+    protected $appends = [ 'url', 'avatar' ];
 
     public function questions()
     {
@@ -38,6 +40,46 @@ class User extends Authenticatable
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+
+    public function posts()
+    {
+        $type = request()->get('type');
+
+        if ( $type === 'questions' ) {
+            $posts = $this->questions()->get();
+        } else {
+            $posts = $this->answers()->with('question')->get();
+            if ( $type !== 'answers' ) {
+                $posts2 = $this->questions()->get();
+
+                $posts = $posts->merge($posts2);
+
+            }
+        }
+        $data = collect();
+
+        foreach ($posts as $post) {
+            $item = [
+                'votes_count' => $post->votes_count,
+                'created_at'  => $post->created_at->format('M d Y')
+            ];
+
+            if ( $post instanceof Answer ) {
+                $item['type'] = 'A';
+                $item['title'] = $post->question->title;
+                $item['accepted'] = $post->question->best_answer_id
+                === $post->id ? true : false;
+            } elseif ( $post instanceof Question ) {
+                $item['type'] = 'Q';
+                $item['title'] = $post->title;
+                $item['accepted'] = (bool)$post->best_answer_id;
+            }
+
+            $data->push($item);
+
+            return $data->sortByDesc('votes_count')->values()->all();
+        }
     }
 
     public function getUrlAttribute()
@@ -51,23 +93,25 @@ class User extends Authenticatable
     {
         $email = $this->email;
         $size = 40;
-        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+        return "https://www.gravatar.com/avatar/" . md5(
+                strtolower(trim($email))
+            ) . "?s=" . $size;
     }
 
     public function favorites()
     {
-        return $this->belongsToMany(Question::class,'favorites')
+        return $this->belongsToMany(Question::class, 'favorites')
             ->withTimestamps();
     }
 
     public function voteQuestions()
     {
-        return $this->morphedByMany(Question::class,'votable');
+        return $this->morphedByMany(Question::class, 'votable');
     }
 
     public function voteAnswers()
     {
-        return $this->morphedByMany(Answer::class,'votable');
+        return $this->morphedByMany(Answer::class, 'votable');
     }
 
     public function voteQuestion(Question $question, $vote)
@@ -81,24 +125,25 @@ class User extends Authenticatable
     {
         $voteAnswers = $this->voteAnswers();
 
-        return $this->_vote($voteAnswers,$answer,$vote);
+        return $this->_vote($voteAnswers, $answer, $vote);
 
     }
 
-    private function _vote($relationship,$model,$vote)
+    private function _vote($relationship, $model, $vote)
     {
 
-        if( $relationship
-            ->where('votable_id',$model->id)
-            ->exists() ){
-            $relationship->updateExistingPivot($model,['vote'=>$vote]);
-        }else{
-            $relationship->attach($model,['vote'=>$vote]);
+        if ( $relationship
+            ->where('votable_id', $model->id)
+            ->exists()
+        ) {
+            $relationship->updateExistingPivot($model, [ 'vote' => $vote ]);
+        } else {
+            $relationship->attach($model, [ 'vote' => $vote ]);
         }
 
         $model->load('votes');
-        $downVotes = (int) $model->downVotes()->sum('vote');
-        $upVotes = (int) $model->upVotes()->sum('vote');
+        $downVotes = (int)$model->downVotes()->sum('vote');
+        $upVotes = (int)$model->upVotes()->sum('vote');
 
         $model->votes_count = $upVotes + $downVotes;
         $model->save();
